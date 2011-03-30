94d5daa1ccae09ed0a8238eda188c5a8154196e3
Jeremy Hanna <jeremy.hanna1234@gmail.com>
2011-03-30 01:28:41 -0500

    Adding a generate UUID udf. Still need to do some testing there.

---
+++ udf/uuid/src/main/java/org/pygmalion/udf/uuid/GenerateTimeUUID.java
@@ -0,0 +1,30 @@
+package org.pygmalion.udf.uuid;
+
+import me.prettyprint.cassandra.utils.TimeUUIDUtils;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Generates a time UUID. Either give it a time to use or
+ * don't specify one and get a time UUID based on the current
+ * time.
+ */
+public class GenerateTimeUUID extends EvalFunc<String> {
+
+    @Override
+    public String exec(Tuple input) throws IOException {
+        String rval = null;
+
+        if (input != null || input.size() == 0) {
+            rval = TimeUUIDUtils.getUniqueTimeUUIDinMillis().toString();
+        } else if (input.size() == 1) {
+            Long time = (Long) input.get(0);
+            rval = TimeUUIDUtils.getTimeUUID(time).toString();
+        }
+
+        return rval;
+    }
+}
