11a1c64fdca17190792488bff245bd588535224b
Jeremy Hanna <jeremy.hanna1234@gmail.com>
2011-04-07 12:51:26 -0500

    Unifying the UDFs into a single jar. Shifting around structure for that.  Still needs some examples.

---
+++ udf/src/main/java/org/pygmalion/udf/FromCassandraBag.java
@@ -0,0 +1,96 @@
+package org.pygmalion.udf;
+
+import java.io.IOException;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DataType;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+/**
+ * UDF to take the data structure that Cassandra outputs:
+ * <code>(key, columns: bag {T: tuple(name, value)})</code>
+ * and projects out just the key and column values that I
+ * would like to use:
+ * <code>bag: {(key, columns: bag {values})}</code>
+ *
+ * We can not only specify column names but also prefixes.
+ * In this case it will return the bag of name/value pairs
+ * in the bag of values that is returned.
+ *
+ * Example:
+ * ('account_id', 'tag*') as a set of fields to project
+ * will return this bag of values:
+ * (12345, {('tag123', 'lol'), ('tag456', 'cat')}
+ *
+ * We are returning a bag when we really want to
+ * be returning a tuple. The reason is that if we
+ * return a tuple then pig wraps it up in another
+ * tuple (which we don't want). By stuffing our
+ * single record result into a bag we can simply
+ * FLATTEN it and get what we want.
+ *
+ * NB: When a row has a ton of columns and if we don't
+ * specify a slice predicate, this will be inefficient.
+ */
+public class FromCassandraBag extends EvalFunc<Tuple> {
+
+    static BagFactory bagFactory     = BagFactory.getInstance();
+    static TupleFactory tupleFactory = TupleFactory.getInstance();
+
+    public Tuple exec(Tuple input) throws IOException {
+        // Size must be two (column_selector,cassandra_bag)
+        if (input == null || input.size() < 2)
+            return null;
+
+        String columnSelector = input.get(0).toString();
+        DataBag cassandraBag  = (DataBag)input.get(1);
+        String[] selections   = columnSelector.split(",");
+        Tuple output          = tupleFactory.getInstance().newTuple(selections.length);
+
+        for (int i = 0; i < selections.length; i++) {
+            String selection = selections[i];
+            if (selection.endsWith("*")) {
+                String namePrefix  = selection.substring(0,selection.length()-1);
+                DataBag columnsBag = bagFactory.newDefaultBag();
+
+                // Find all columns in the input bag that begin with 'namePrefix'
+                // and add them to the 'columnsBag'
+                for (Tuple cassandraColumn : cassandraBag) {
+                    String name = cassandraColumn.get(0).toString();
+                    if (name.startsWith(namePrefix)) {
+                        columnsBag.add(cassandraColumn);
+                    }
+                }
+
+                // Sometimes this bag will have no columns in it, this _is_ the desired behavior.
+                output.set(i, columnsBag);
+
+            } else {
+
+                // Find the column in the input bag that has a name equal to 'selection'
+                // and add _only_ the value to the output tuple. This is what you actually
+                // want since you're specifying both an order and a name in the 'columnSelector'
+                // string.
+                for (Tuple cassandraColumn : cassandraBag) {
+                    String name = cassandraColumn.get(0).toString();
+                    if (name.equals(selection)) {
+                        output.set(i, cassandraColumn.get(1));
+                        break;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+    public Schema outputSchema(Schema input) {
+        try {
+            return new Schema(new Schema.FieldSchema(getSchemaName(this.getClass().getName().toLowerCase(), input), DataType.TUPLE));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
---
+++ udf/src/main/java/org/pygmalion/udf/ToCassandraBag.java
@@ -0,0 +1,78 @@
+package org.pygmalion.udf;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.util.UDFContext;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * EvalFunc to take the given set of values and convert them
+ * into a CassandraBag for persisting back to Cassandra.
+ *
+ * The first value in the input bag *has* to be the key. For
+ * the rest of the fields, this UDF will interrogate the values
+ * that you have named the variables to be the column names.
+ * If you have bags of (name, value) tuples as elements of the
+ * input bag, it will just add all of their tuples to the bag to
+ * be persisted individually.
+ */
+public class ToCassandraBag extends EvalFunc<Tuple> {
+
+    private static String UDFCONTEXT_SCHEMA_KEY = "cassandra.input_field_schema";
+    private static String DELIM = ",";
+
+    public Tuple exec(Tuple input) throws IOException {
+        Tuple row = TupleFactory.getInstance().newTuple(2);
+        DataBag columns = BagFactory.getInstance().newDefaultBag();
+        UDFContext context = UDFContext.getUDFContext();
+        Properties property = context.getUDFProperties(ToCassandraBag.class);
+        String fieldString = property.getProperty(UDFCONTEXT_SCHEMA_KEY);
+        String [] fieldnames = fieldString.split(DELIM);
+
+        // IT IS ALWAYS ASSUMED THAT THE OBJECT AT INDEX 0 IS THE ROW KEY
+
+        for (int i=1; i<input.size(); i++) {
+            if (input.get(i) instanceof DataBag) {
+                columns.addAll((DataBag) input.get(i));
+            } else {
+                columns.add(getColumnDef(fieldnames[i], input.get(i)));
+            }
+        }
+
+        row.set(0, input.get(0));
+        row.set(1, columns);
+        return row;
+    }
+
+    private Tuple getColumnDef(String name, Object value) throws ExecException {
+        Tuple column = TupleFactory.getInstance().newTuple(2);
+        column.set(0, name);
+        column.set(1, value);
+        return column;
+    }
+
+    public Schema outputSchema(Schema input) {
+        StringBuilder builder = new StringBuilder();
+        List<Schema.FieldSchema> fields = input.getFields();
+        for (int i=0; i<fields.size(); i++) {
+            builder.append(fields.get(i).alias);
+            if (i != fields.size()-1) {
+                builder.append(DELIM);
+            }
+        }
+
+        UDFContext context = UDFContext.getUDFContext();
+        Properties property = context.getUDFProperties(ToCassandraBag.class);
+        property.setProperty(UDFCONTEXT_SCHEMA_KEY, builder.toString());
+
+        return super.outputSchema(input);
+    }
+}
