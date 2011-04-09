94fe0916832743c9d4a4af798b176acc519f83e7
Jeremy Hanna <jeremy.hanna1234@gmail.com>
2011-04-08 19:17:20 -0500

    Adding the contents of the ConcatMany udf.

--- udf/src/main/java/org/pygmalion/udf/ConcatMany.java
+++ udf/src/main/java/org/pygmalion/udf/ConcatMany.java
@@ -1,4 +1,21 @@
 package org.pygmalion.udf;
 
-public class ConcatMany {
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+import java.io.IOException;
+
+public class ConcatMany extends EvalFunc<String> {
+    @Override
+    public String exec(Tuple input) throws IOException {
+        if (input == null || input.size() == 0) {
+            return null;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (Object o : input.getAll()) {
+            builder.append((String) o);
+        }
+
+        return builder.toString();
+    }
 }
