b3028e70759dd1957ea9b6f9a1fb4c4c6eafa415
Grant Ingersoll <grant.ingersoll@gsi-dachis.local>
2011-07-02 11:19:41 -0400

    More testing

--- udf/src/test/java/org/pygmailion/udf/ToCassandraBagTest.java
+++ udf/src/test/java/org/pygmailion/udf/ToCassandraBagTest.java
@@ -1,6 +1,7 @@
 package org.pygmailion.udf;
 
 
+import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DefaultTuple;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.util.UDFContext;
@@ -8,9 +9,12 @@ import org.eclipse.jdt.internal.compiler.codegen.AttributeNamesConstants;
 import org.junit.Test;
 import org.pygmalion.udf.ToCassandraBag;
 
+import java.io.IOException;
 import java.util.Properties;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -36,5 +40,38 @@ public class ToCassandraBagTest {
         properties.setProperty(ToCassandraBag.UDFCONTEXT_SCHEMA_KEY, builder.toString());
         Tuple tuple = tcb.exec(input);
         assertNotNull("Tuple is null", tuple);
+        assertEquals(2, tuple.size());
+        //first is the key, rest is a set of columns
+        Object one = tuple.get(0);
+        assertTrue(one instanceof String);
+        Object two = tuple.get(1);
+        assertTrue(two instanceof DataBag);
+        //Bad input
+        input = new DefaultTuple();
+        input.append(null);
+        input.append("foo");
+        try {
+            tcb.exec(input);
+            assertTrue(false);
+        } catch (IOException e) {
+            //expected
+        }
+        input = new DefaultTuple();
+        builder.setLength(0);
+        for (int i = 0; i < fields.length -1; i++){
+            builder.append(fields[i]);
+            input.append("foo" + i);
+            if (i < fields.length - 1){
+                builder.append(',');
+            }
+        }
+        properties.setProperty(ToCassandraBag.UDFCONTEXT_SCHEMA_KEY, builder.toString());
+        input.append("foo extra");
+        try {
+            tcb.exec(input);
+            assertTrue(false);
+        } catch (IOException e) {
+
+        }
     }
 }
